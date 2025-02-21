4f8eaa7ed2418ba4d0600d74220e242c9ced27fd
ThangHND <yuuwhisky@gmail.com>
2025-02-21 15:48:54 +0700

    register p1

--- src/main/java/com/example/laptopshop/controller/client/HomePageController.java
+++ src/main/java/com/example/laptopshop/controller/client/HomePageController.java
@@ -2,19 +2,29 @@ package com.example.laptopshop.controller.client;
 
 import java.util.List;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.laptopshop.domain.Product;
+import com.example.laptopshop.domain.User;
+import com.example.laptopshop.domain.dto.RegisterDTO;
 import com.example.laptopshop.service.ProductService;
+import com.example.laptopshop.service.UserService;
 
 @Controller
 public class HomePageController {
     private final ProductService productService;
+    private final UserService userService;
+    private PasswordEncoder passwordEncoder;
 
-    public HomePageController(ProductService productService) {
+    public HomePageController(ProductService productService, UserService userService, PasswordEncoder passwordEncoder) {
         this.productService = productService;
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/")
@@ -24,4 +34,30 @@ public class HomePageController {
         return "client/HomePage/show";
     }
 
+    @GetMapping("/register")
+    public String showPageRegister(Model model) {
+        model.addAttribute("registerUser", new RegisterDTO());
+        return "/client/auth/register";
+    }
+
+    @PostMapping("/register")
+    public String handleRegister(@ModelAttribute("registerUser") RegisterDTO dto) {
+        User user = this.userService.registerDTOtoUser(dto);
+
+        String hashPassword = passwordEncoder.encode(user.getPassword());
+
+        user.setPassword(hashPassword);
+
+        user.setRole(this.userService.getRoleByName("USER"));
+
+        this.userService.saveUser(user);
+
+        return "redirect:/login";
+
+    }
+
+    @GetMapping("/login")
+    public String showPageLogin(Model model) {
+        return "/client/auth/login";
+    }
 }
